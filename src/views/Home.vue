036282df3fd78e539c29620174a7fb8cc14a54be
Meruem117 <1572677319@qq.com>
2023-04-21 23:53:15 +0800

    add less

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="home">
+    <div class="page">
     </div>
 </template>
 
@@ -13,3 +13,11 @@ import { Vue, Component } from 'vue-property-decorator'
 
 export default class HomeView extends Vue { }
 </script>
+
+<style lang="less" scoped>
+.page {
+    width: 100%;
+    min-height: 100vh;
+    background-color: #e7e9fa;
+}
+</style>
