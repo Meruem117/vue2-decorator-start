e06cbc135d44bf5d2428a2db8456af1b0684425c
Meruem117 <1572677319@qq.com>
2023-04-21 23:50:13 +0800

    change port

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 3000
+  }
 })
