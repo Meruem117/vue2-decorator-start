8f67534aaf6506a52b5e8bb0193d0bea48baf58f
Meruem117 <1572677319@qq.com>
2023-06-25 21:33:02 +0800

    add pie

---
+++ src/components/ChartPie.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+        <div class="chart" :id="chartId"></div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import * as echarts from 'echarts'
+
+@Component
+export default class ChartLine extends Vue {
+    @Prop({
+        default: '',
+        required: true
+    })
+    code!: string
+
+    get chartId() {
+        return 'chart-pie-' + this.code
+    }
+
+    get config() {
+        return {
+
+        }
+    }
+
+    chart: any = null
+
+    init() {
+        this.$nextTick(() => {
+            this.drawChart()
+        })
+    }
+
+    handleResize() {
+        this.$nextTick(() => {
+            if (this.chart) this.chart.resize()
+        })
+    }
+
+    drawChart() {
+        this.chart = echarts.init(document.getElementById(this.chartId)!)
+        let option = this.getOption()
+        this.chart.setOption(option, true)
+        this.handleResize()
+    }
+
+    getOption() {
+        let option = {
+        }
+        return option
+    }
+}
+</script>
