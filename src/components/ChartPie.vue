31baab1ebf46171a429662101a83be65b828af71
Meruem117 <1572677319@qq.com>
2023-06-26 21:59:52 +0800

    add pie

--- src/components/ChartPie.vue
+++ src/components/ChartPie.vue
@@ -16,6 +16,12 @@ export default class ChartLine extends Vue {
     })
     code!: string
 
+    @Prop({
+        default: () => [],
+        required: false
+    })
+    data!: Chart.PieDataItem[]
+
     get chartId() {
         return 'chart-pie-' + this.code
     }
@@ -42,13 +48,64 @@ export default class ChartLine extends Vue {
 
     drawChart() {
         this.chart = echarts.init(document.getElementById(this.chartId)!)
-        let option = this.getOption()
+        let option = this.getOption(this.data)
         this.chart.setOption(option, true)
         this.handleResize()
     }
 
-    getOption() {
+    getOption(data: Chart.PieDataItem[]) {
+        let colorList = ['#518dff', '#00e2f0', '#08d066', '#ffd200', '#ffa800', '#ff6271', '#ffb271', '#ffc271']
+        let dataOuter: any[] = [], dataInner: any[] = [], total = 0
+        data.forEach((item, index) => {
+            total += item.value || 0
+            dataOuter.push({
+                name: item.name,
+                value: item.value || 0,
+                rate: item.rate || '',
+                itemStyle: {
+                    color: colorList[index % colorList.length],
+                }
+            })
+            dataInner.push({
+                name: item.name,
+                value: item.value,
+                rate: item.rate,
+                itemStyle: {
+                    color: colorList[index % colorList.length],
+                    opacity: .3
+                }
+            })
+        })
         let option = {
+            grid: {
+                top: 10,
+                bottom: 10,
+                left: 10,
+                right: 10,
+            },
+            series: [
+                {
+                    type: 'pie',
+                    radius: ['52%', '68%'],
+                    label: {
+                        normal: {
+                            show: false
+                        },
+                        emphasis: {
+                            show: false
+                        }
+                    },
+                    labelLine: {
+                        normal: {
+                            show: false
+                        },
+                        emphasis: {
+                            show: false
+                        }
+                    },
+                    data: dataOuter,
+                },
+            ]
         }
         return option
     }
